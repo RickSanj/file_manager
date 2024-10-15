392130d43f638611954180510ad8dda505792e08
Olena Azarova <116667018+olenaazarova@users.noreply.github.com>
2024-10-15 16:02:09 +0300

    add copy paste

--- FileManager/mainwindow.cpp
+++ FileManager/mainwindow.cpp
@@ -87,8 +87,8 @@ void MainWindow::handleCustomContextMenuRequested(const QPoint &pos)
         QAction *openAction = contextMenu.addAction("Open");
         connect(openAction, &QAction::triggered, this, &MainWindow::handleOpenActionTriggered);
     }
-
-
+    QAction *copyAction = contextMenu.addAction("Copy");
+    QAction *pasteAction = contextMenu.addAction("Paste");
     QAction *deleteAction = contextMenu.addAction("Delete");
     connect(deleteAction, &QAction::triggered, this, &MainWindow::handleDeleteTriggered);
 
@@ -96,6 +96,11 @@ void MainWindow::handleCustomContextMenuRequested(const QPoint &pos)
     QAction *renameAction = contextMenu.addAction("Rename");
     connect(renameAction, &QAction::triggered, this, &MainWindow::renameItem);
 
+    if (copyPath.isEmpty()) {
+        pasteAction->setDisabled(true);
+    }
+    connect(copyAction, &QAction::triggered, this, &MainWindow::onCopyTriggered);
+    connect(pasteAction, &QAction::triggered, this, &MainWindow::onPasteTriggered);
 
     contextMenu.exec(ui->treeView->viewport()->mapToGlobal(pos));
 }
@@ -171,3 +176,95 @@ void MainWindow::handleOpenActionTriggered()
     }
     handleTreeViewDoubleClicked(index);
 }
+
+void MainWindow::onCopyTriggered(){
+    QString sourcePath = model->filePath(currentIndex);
+    QFileInfo fileInfo(sourcePath);
+
+    if (!fileInfo.exists()) {
+        QMessageBox::warning(this, "Copy", "No file or directory selected.");
+        return;
+    }
+
+    copyPath = sourcePath;
+}
+
+void MainWindow::onPasteTriggered() {
+    if (copyPath.isEmpty()) {
+        QMessageBox::warning(this, "Paste", "No file or directory to paste.");
+        return;
+    }
+
+    QString destinationPath = model->filePath(currentIndex);
+    QFileInfo destInfo(destinationPath);
+
+    if (!destInfo.isDir()) {
+        QMessageBox::warning(this, "Paste", "Please select a valid folder to paste into.");
+        return;
+    }
+
+    // Get the file name from the source and create a new path in the destination folder
+    QFileInfo sourceInfo(copyPath);
+    QString newFilePath = destinationPath + "/" + sourceInfo.fileName();
+
+    // Check if the file already exists in the destination
+    if (QFile::exists(newFilePath)) {
+        QMessageBox::StandardButton reply = QMessageBox::question(
+            this, "Overwrite File", "The file already exists. Do you want to overwrite it?",
+            QMessageBox::Yes | QMessageBox::No
+            );
+
+        if (reply == QMessageBox::No) {
+            return;  // Abort paste operation if user chooses not to overwrite
+        }
+    }
+
+    if (sourceInfo.isFile()) {
+        // Copy the file
+        if (QFile::copy(copyPath, newFilePath)) {
+            QMessageBox::information(this, "Paste", "File pasted successfully.");
+        } else {
+            QMessageBox::warning(this, "Paste", "Failed to paste the file.");
+        }
+    } else if (sourceInfo.isDir()) {
+        // Copy directory recursively
+        QDir sourceDir(copyPath);
+        QDir targetDir(destinationPath);
+        if (copyDirectory(sourceDir, targetDir)) {
+            QMessageBox::information(this, "Paste", "Directory pasted successfully.");
+        } else {
+            QMessageBox::warning(this, "Paste", "Failed to paste the directory.");
+        }
+    }
+
+    // Clear the copy path after pasting
+    copyPath.clear();
+}
+
+bool MainWindow::copyDirectory(QDir sourceDir, QDir targetDir)
+{
+    if (!targetDir.exists()) {
+        targetDir.mkdir(targetDir.path());
+    }
+
+    foreach (QString fileName, sourceDir.entryList(QDir::Files)) {
+        QString sourceFilePath = sourceDir.filePath(fileName);
+        QString targetFilePath = targetDir.filePath(fileName);
+        if (!QFile::copy(sourceFilePath, targetFilePath)) {
+            return false;
+        }
+    }
+
+    foreach (QString subDirName, sourceDir.entryList(QDir::Dirs | QDir::NoDotAndDotDot)) {
+        QString sourceSubDirPath = sourceDir.filePath(subDirName);
+        QString targetSubDirPath = targetDir.filePath(subDirName);
+        QDir sourceSubDir(sourceSubDirPath);
+        QDir targetSubDir(targetSubDirPath);
+        if (!copyDirectory(sourceSubDir, targetSubDir)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
